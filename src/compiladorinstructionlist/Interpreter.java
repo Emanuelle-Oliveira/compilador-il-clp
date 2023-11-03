91e1a5536f3d25db34d701d2d249585216ff850b
Emanuelle Oliveira <emanuelletrindadeoliveira@gmail.com>
2023-11-03 09:43:43 -0300

    feat: separate operator and variable

--- src/compiladorinstructionlist/Interpreter.java
+++ src/compiladorinstructionlist/Interpreter.java
@@ -7,13 +7,40 @@ public class Interpreter {
     
     public static Map receiveLines(List<String> lineList, Map outputs) {
         
+        char character = '-';
+        //char lastCharacter = '-';
+        //Boolean lineIsCorrect = true;
+        Boolean spaceDetected = false;
+        //Boolean skipLineDetected = false;
+        String operator = "";
+        String variable = "";
+        
         for (int i = 0; i < lineList.size(); i++) {
             for (int j = 0; j < lineList.get(i).length(); j++) {
-                char character = lineList.get(i).charAt(j);
-                System.out.println(character);
+                
+                //lastCharacter = character;
+
+                character = lineList.get(i).charAt(j);
+                
+                if (character != ' ' && character != '\n' && !spaceDetected) {
+                    operator = operator + character;
+                } 
+                
+                if(character == ' ') {
+                    spaceDetected = true;
+                }
+                
+                if (character != ' ' && character != '\n' && spaceDetected) {
+                    variable = variable + character;
+                }
             }
+            System.out.println("Operador: " + operator);
+            System.out.println("Variável: " + variable);
+            
+            spaceDetected = false;
+            operator = "";
+            variable = "";
         }
-        
         return outputs;
     }
 }
